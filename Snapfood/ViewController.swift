ce280c484623ad642931a2a3c917c3e585385335
Likhit <likhitgmella@gmail.com>
2020-09-05 20:29:42 +0530

    project setup

---
+++ Snapfood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 05/09/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
