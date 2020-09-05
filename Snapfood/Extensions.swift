4f9628ab601d303c5d4b295021e6543813c6ab82
Likhit <likhitgmella@gmail.com>
2020-09-05 20:44:22 +0530

    keyboard & button extensions

---
+++ Snapfood/Extensions.swift
@@ -0,0 +1,48 @@
+//
+//  Extensions.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 05/09/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class Extensions: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
+// Dismiss keyboard on tapping anywhere
+extension UIViewController {
+    
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+}
+
+// Button hover
+extension UIButton {
+    
+    func flash() {
+        let flash = CABasicAnimation(keyPath: "opacity")
+        flash.duration = 0.1
+        flash.fromValue = 1
+        flash.toValue = 0.5
+        flash.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        flash.autoreverses = true
+        flash.repeatCount = 1
+        layer.add(flash, forKey: nil)
+    }
+    
+}   // #49
