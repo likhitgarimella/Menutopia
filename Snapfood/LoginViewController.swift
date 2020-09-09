0f69f10489ad929b43c573cea35f5509ab7466cf
Likhit <likhitgmella@gmail.com>
2020-09-09 15:11:45 +0530

    2 new VCs added

---
+++ Snapfood/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 09/09/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
---
+++ Snapfood/SelectSignupViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SelectSignupViewController.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 09/09/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class SelectSignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
