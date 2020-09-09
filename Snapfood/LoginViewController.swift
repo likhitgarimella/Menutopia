6af8d0b1b17eb2612dbdf27015d14678381f2763
Likhit <likhitgmella@gmail.com>
2020-09-09 23:37:07 +0530

    login screen textfields

--- Snapfood/LoginViewController.swift
+++ Snapfood/LoginViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet var email: UITextField!
+    @IBOutlet var password: UITextField!
+    
+    func Properties() {
+        
+        email.backgroundColor = UIColor(red: 252/255, green: 239/255, blue: 238/255, alpha: 1)
+        password.backgroundColor = UIColor(red: 252/255, green: 239/255, blue: 238/255, alpha: 1)
+        
+    }
+    
+    func CornerRadius() {
+        
+        email.layer.cornerRadius = 20
+        password.layer.cornerRadius = 20
+        
+    }
+    
+    func LeftPadding() {
+        
+        email.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: email.frame.height))
+        email.leftViewMode = .always
+        password.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: password.frame.height))
+        password.leftViewMode = .always
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        hideKeyboardWhenTappedAround()
         
+        Properties()
+        CornerRadius()
+        LeftPadding()
         
     }
     
-}   // #21
+}   // #52
