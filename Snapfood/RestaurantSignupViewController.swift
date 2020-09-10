6ad3498d417cfbc90fbd578ba5a78ebd25684eb6
Likhit <likhitgmella@gmail.com>
2020-09-10 07:40:36 +0530

    signup 2 new VC files

---
+++ Snapfood/RestaurantSignupViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RestaurantSignupViewController.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 10/09/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantSignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
---
+++ Snapfood/UserSignupViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UserSignupViewController.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 10/09/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class UserSignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
