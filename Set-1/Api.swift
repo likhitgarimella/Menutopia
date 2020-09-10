04c717b4765efd9287de3f57a54ee1e7d930a70c
Likhit <likhitgmella@gmail.com>
2020-09-10 10:09:43 +0530

    api.swift

---
+++ Set-1/Api.swift
@@ -0,0 +1,19 @@
+//
+//  Api.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 10/09/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import Foundation
+
+// Place to store all Apis
+
+struct Api {
+    
+    // MARK:- User
+    
+    static var UserDet = UserApi()
+    
+}   // #20
