996f2c5cafea5f4e04c1000df3b4e455400aa83d
likhitgarimella <likhitgmella@gmail.com>
2020-10-03 14:36:14 +0530

    2 vc files and classes

---
+++ Set-2/RestaurantPostViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RestaurantPostViewController.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 03/10/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
---
+++ Set-2/UserPostViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UserPostViewController.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 03/10/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class UserPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
