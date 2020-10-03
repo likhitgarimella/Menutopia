2c8a9a563884b8a1410279bce4bc0ca8d1e55d5e
likhitgarimella <likhitgmella@gmail.com>
2020-10-03 14:14:20 +0530

    seg ctrl prop & actions; cont views

--- Set-2/PostViewController.swift
+++ Set-2/PostViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class PostViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet var segment: UISegmentedControl!
+    @IBOutlet var restaurantView: UIView!
+    @IBOutlet var userView: UIView!
+    
+    func SegmentFontColor() {
+        
+        // Selected option color
+        segment.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.darkGray], for: .selected)
+        // Color of other options
+        segment.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.darkGray], for: .normal)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        hideKeyboardWhenTappedAround()
+        SegmentFontColor()
         
     }
     
-}   // #21
+    // Switch Index of Segmented Control
+    @IBAction func switchSegment(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 {
+            restaurantView.alpha = 1
+            userView.alpha = 0
+        }
+        if sender.selectedSegmentIndex == 1 {
+            restaurantView.alpha = 0
+            userView.alpha = 1
+        }
+        
+    }
+    
+}   // #50
