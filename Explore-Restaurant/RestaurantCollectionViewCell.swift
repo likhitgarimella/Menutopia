de24fe588947786f1954b6d6fb60778a4de613ad
likhitgarimella <likhitgmella@gmail.com>
2020-10-10 23:30:11 +0530

    like image; xib modfns & addns; xib outlets connected to file

--- Explore-Restaurant/RestaurantCollectionViewCell.swift
+++ Explore-Restaurant/RestaurantCollectionViewCell.swift
@@ -7,8 +7,30 @@
 //
 
 import UIKit
+// import Firebase
+
+/// If a View needs data, it should ask controllers...
 
 class RestaurantCollectionViewCell: UICollectionViewCell {
+    
+    // Outlets
+    @IBOutlet var cardView: UIView!
+    @IBOutlet var widthConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var restProfilePic: UIImageView!
+    @IBOutlet weak var restName: UILabel!
+    @IBOutlet weak var restDetails: UILabel!
+    @IBOutlet weak var restMenuItem: UIImageView!
+    @IBOutlet weak var restMenuDesc: UILabel!
+    @IBOutlet weak var restItemPrice: UILabel!
+    
+    @IBOutlet weak var tag1: UILabel!
+    @IBOutlet weak var tag2: UILabel!
+    @IBOutlet weak var tag3: UILabel!
+    
+    @IBOutlet var restLikeImageView: UIImageView!
+    @IBOutlet var likeCountButton: UIButton!
+    @IBOutlet weak var timestampValue: UILabel!
     
     // linking feed VC & post cell
     var restaurantFeedVC: ExploreRestaurantViewController?
@@ -46,4 +68,4 @@ class RestaurantCollectionViewCell: UICollectionViewCell {
         
     }
 
-}   // #50
+}   // #72
