2a696f903ddb4490a610d4793301758382ea99d4
likhitgarimella <likhitgmella@gmail.com>
2020-10-10 23:10:43 +0530

    placeholder image; explore rest vc delg fns; rest feed cell xib storyboard; rest feed cell file decs

--- Explore-Restaurant/ExploreRestaurantViewController.swift
+++ Explore-Restaurant/ExploreRestaurantViewController.swift
@@ -7,8 +7,49 @@
 //
 
 import UIKit
+// import Firebase
+// import FirebaseDatabase
 
-class ExploreRestaurantViewController: UIViewController {
+class ExploreRestaurantViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet var restaurantFeedCollectionView: UICollectionView!
+    
+    @IBOutlet var activityIndicatorView: UIActivityIndicatorView!
+    
+    // reference to store MealPostModel class info
+    var restaurantPosts = [MealPostModel]()
+    
+    // reference to store User class info
+    var users = [AppUser]()
+    
+    // numberOfItemsInSection
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return restaurantPosts.count
+    }
+    
+    // cellForItemAt
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        /// Timestamp
+        /* if let seconds = mentor.timestamp {
+            // let timeStampDate = NSDate(timeIntervalSince1970: seconds)
+            let pastDate = Date(timeIntervalSince1970: seconds)
+            // let dateFormatter = DateFormatter()
+            // dateFormatter.dateFormat = "MMM d, h:mm a"
+            // cell.timeAgo.text = dateFormatter.string(from: timeStampDate as Date)
+            // cell.timeAgo.text = pastDate.timeAgoDisplay()
+        } */
+        
+        let restaurantCell = restaurantFeedCollectionView.dequeueReusableCell(withReuseIdentifier: "RestaurantCollectionViewCell", for: indexPath) as! RestaurantCollectionViewCell
+        let post = restaurantPosts[indexPath.row]
+        let user = users[indexPath.row]
+        restaurantCell.restaurantPost = post
+        restaurantCell.user = user
+        // linking home VC & home table view cell
+        restaurantCell.restaurantFeedVC = self
+        return restaurantCell
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,4 +58,4 @@ class ExploreRestaurantViewController: UIViewController {
         
     }
     
-}   // #21
+}   // #62
--- Explore-Restaurant/RestaurantCollectionViewCell.swift
+++ Explore-Restaurant/RestaurantCollectionViewCell.swift
@@ -9,7 +9,36 @@
 import UIKit
 
 class RestaurantCollectionViewCell: UICollectionViewCell {
-
+    
+    // linking feed VC & post cell
+    var restaurantFeedVC: ExploreRestaurantViewController?
+    
+    var restaurantPost: MealPostModel? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    /// when this user property is set..
+    /// we'll let the cell download the correspoding cell..
+    var user: AppUser? {
+        didSet {
+            setupUserInfo()
+        }
+    }
+    
+    func updateView() {
+        
+        
+        
+    }
+    
+    func setupUserInfo() {
+        
+        
+        
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -17,4 +46,4 @@ class RestaurantCollectionViewCell: UICollectionViewCell {
         
     }
 
-}   // #21
+}   // #50
