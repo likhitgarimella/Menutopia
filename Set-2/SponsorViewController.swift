d982053f4594db97e4faa6c0ff9549f659c49298
likhitgarimella <likhitgmella@gmail.com>
2020-10-10 14:02:09 +0530

    sponsor screen done

---
+++ Set-2/SponsorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SponsorViewController.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 10/10/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class SponsorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
