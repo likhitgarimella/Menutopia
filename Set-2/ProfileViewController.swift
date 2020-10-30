0426fc49c1b391ea15a119893d268ea77728d5e6
likhitgarimella <likhitgmella@gmail.com>
2020-10-30 09:51:09 +0530

    rest & user profile VCs seprtd

---
+++ Set-2/ProfileViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ProfileViewController.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 30/10/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet var segment: UISegmentedControl!
+    @IBOutlet var restaurantProfile: UIView!
+    @IBOutlet var userProfile: UIView!
+    
+    func SegmentFontColor() {
+        
+        // Selected option color
+        segment.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.darkGray], for: .selected)
+        // Color of other options
+        segment.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.darkGray], for: .normal)
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        hideKeyboardWhenTappedAround()
+        SegmentFontColor()
+        
+    }
+    
+    // Switch Index of Segmented Control
+    @IBAction func switchSegment(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 {
+            restaurantProfile.alpha = 1
+            userProfile.alpha = 0
+        }
+        if sender.selectedSegmentIndex == 1 {
+            restaurantProfile.alpha = 0
+            userProfile.alpha = 1
+        }
+        
+    }
+    
+}   // #50
