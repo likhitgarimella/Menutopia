dd39fafa42cf9be608a3924d7123e7b95c7fb9d1
likhitgarimella <likhitgmella@gmail.com>
2020-10-31 18:08:01 +0530

    feed api done

--- Post-User/FeedApi.swift
+++ Post-User/FeedApi.swift
@@ -7,7 +7,31 @@
 //
 
 import Foundation
+import Firebase
 
 class FeedApi {
     
-}   // #14
+    var REF_FEED = Database.database().reference().child("Feed")
+    
+    func observeFeed(withId id: String, completion: @escaping (UserPostModel) -> Void) {
+        REF_FEED.child(id).observe(.childAdded, with: {
+            snapshot in
+            let key = snapshot.key
+            Api.UserPost.observePost(withId: key, completion: {
+                (post) in
+                completion(post)
+            })
+        })
+    }
+    
+    func observeFeedRemoved(withId id: String, completion: @escaping (UserPostModel) -> Void) {
+        REF_FEED.child(id).observe(.childRemoved, with: {
+            snapshot in
+            let key = snapshot.key
+            Api.UserPost.observePost(withId: key) { (post) in
+                completion(post)
+            }
+        })
+    }
+    
+}   // #38
