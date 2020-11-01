2da6cf824f2e67ca0ddf4290d23386a59a3af983
likhitgarimella <likhitgmella@gmail.com>
2020-11-01 08:26:35 +0530

    segment and cont VCs in discover VC

--- Segments/DiscoverViewController.swift
+++ Segments/DiscoverViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class DiscoverViewController: UIViewController {
 
+    // Outlets
+    @IBOutlet var segment: UISegmentedControl!
+    @IBOutlet var discoverRestaurants: UIView!
+    @IBOutlet var discoverUsers: UIView!
+    
+    func SegmentFontColor() {
+        
+        // Selected option color
+        segment.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.darkGray], for: .selected)
+        // Color of other options
+        segment.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.darkGray], for: .normal)
+        
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        hideKeyboardWhenTappedAround()
+        SegmentFontColor()
         
     }
     
-}   // #21
+    // Switch Index of Segmented Control
+    @IBAction func switchSegment(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 {
+            discoverRestaurants.alpha = 1
+            discoverUsers.alpha = 0
+        }
+        if sender.selectedSegmentIndex == 1 {
+            discoverRestaurants.alpha = 0
+            discoverUsers.alpha = 1
+        }
+        
+    }
+    
+}   // #50
