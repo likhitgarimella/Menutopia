074d3232e8167b334bb14dfa487eb120c170a3b1
likhitgarimella <likhitgmella@gmail.com>
2020-11-01 15:57:42 +0530

    Properties, handleSelectPhoto, handlePost, viewWillAppear

--- Post-User/UserPostViewController.swift
+++ Post-User/UserPostViewController.swift
@@ -38,12 +38,70 @@ class UserPostViewController: UIViewController, UITextViewDelegate {
     
     // progress hud
     let hud1 = JGProgressHUD(style: .dark)
+    
+    func Properties() {
+        
+        shareOutlet.layer.cornerRadius = 6
+        
+        captionTextView.backgroundColor = UIColor.white
+        // text view delegate
+        captionTextView.delegate = self
+        // acts as default text view placeholder
+        captionTextView.text = "Write a caption..."
+        captionTextView.textColor = UIColor.lightGray
+        
+        // Add gesture for profile image present in screen
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleSelectPhoto))
+        photo.addGestureRecognizer(tapGesture)
+        photo.isUserInteractionEnabled = true
+        
+    }
+    
+    @objc func handleSelectPhoto() {
+        
+        let pickerController = UIImagePickerController()
+        // To get access to selected media files, add delegate
+        pickerController.delegate = self
+        /// presenting it in full screen bcuz...
+        /// i want the view to change...
+        /// so that viewWillAppear will work...
+        pickerController.modalPresentationStyle = .fullScreen
+        // present photo library
+        present(pickerController, animated: true, completion: nil)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         hideKeyboardWhenTappedAround()
+        Properties()
         
     }
     
-}   // #50
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        handlePost()
+        
+    }
+    
+    func handlePost() {
+        
+        if selectedImage != nil {
+            // Enable
+            self.removeOutlet.isEnabled = true
+            self.shareOutlet.isEnabled = true
+            shareOutlet.setTitleColor(UIColor.white, for: .normal)
+            shareOutlet.backgroundColor = UIColor(red: 80/255, green: 101/255, blue: 161/255, alpha: 1)
+        } else {
+            // Disable
+            self.removeOutlet.isEnabled = false
+            self.shareOutlet.isEnabled = false
+            shareOutlet.setTitleColor(UIColor.lightText, for: .normal)
+            shareOutlet.backgroundColor = UIColor(red: 80/255, green: 101/255, blue: 161/255, alpha: 1)
+        }
+        
+    }
+    
+}   // #108
