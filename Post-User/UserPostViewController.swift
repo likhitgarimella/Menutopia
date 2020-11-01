5e296f0bf4b20a7c671978e6c7152933bad799fc
likhitgarimella <likhitgmella@gmail.com>
2020-11-01 15:55:38 +0530

    outlets, connections, text view delg fns

--- Post-User/UserPostViewController.swift
+++ Post-User/UserPostViewController.swift
@@ -7,8 +7,37 @@
 //
 
 import UIKit
+// import Firebase
+import JGProgressHUD
 
-class UserPostViewController: UIViewController {
+class UserPostViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet var photo: UIImageView!
+    @IBOutlet var captionTextView: UITextView!
+    @IBOutlet var shareOutlet: UIButton!
+    
+    @IBOutlet var removeOutlet: UIButton!
+    
+    var selectedImage: UIImage?
+    
+    // Delegate function
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    // Delegate function
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "Write a caption..."
+            textView.textColor = UIColor.lightGray
+        }
+    }
+    
+    // progress hud
+    let hud1 = JGProgressHUD(style: .dark)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,4 +46,4 @@ class UserPostViewController: UIViewController {
         
     }
     
-}   // #21
+}   // #50
