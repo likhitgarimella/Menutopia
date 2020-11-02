001da2775e75b3e67f79b58aa18707c6f4d445a5
likhitgarimella <likhitgmella@gmail.com>
2020-11-02 14:28:33 +0530

    table view cell file

---
+++ Discover-People/PeopleTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  PeopleTableViewCell.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 02/11/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class PeopleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        
+        
+    }
+
+}   // #28
