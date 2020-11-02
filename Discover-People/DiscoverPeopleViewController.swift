00211e58e1f14048b0e7dc425bbb9ced80f88fd7
likhitgarimella <likhitgmella@gmail.com>
2020-11-02 13:50:06 +0530

    2 VC files and their classes

---
+++ Discover-People/DiscoverPeopleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DiscoverPeopleViewController.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 02/11/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverPeopleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
---
+++ Discover-Restaurants/DiscoverRestaurantsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DiscoverRestaurantsViewController.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 02/11/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverRestaurantsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
