0e223af0d74ae51bf121da3ce14e312272af4504
likhitgarimella <likhitgmella@gmail.com>
2020-11-03 16:09:44 +0530

    feed VC code done

--- Explore-User/FeedViewController.swift
+++ Explore-User/FeedViewController.swift
@@ -41,4 +41,73 @@ class FeedViewController: UIViewController {
         
     }
     
-}   // #45
+    func loadPosts() {
+        
+        // start when loadPosts func starts
+        activityIndicatorView.startAnimating()
+        
+        Api.Feed.observeFeed(withId: Api.UserDet.CURRENT_USER!.uid, completion: {
+            (post) in
+            guard let postId = post.uid else {
+                return
+            }
+            self.fetchUser(uid: postId, completed: {
+                self.posts.append(post)
+                // print(self.posts)
+                // stop before tablew view reloads data
+                self.activityIndicatorView.stopAnimating()
+                self.activityIndicatorView.hidesWhenStopped = true
+                self.tableView.reloadData()
+            })
+        })
+        
+        /// Remove feed posts (on unfollow)
+        Api.Feed.observeFeedRemoved(withId: Api.UserDet.CURRENT_USER!.uid, completion: {
+            (post) in
+            // print(key)
+            /* for (index, post) in self.posts.enumerated() {
+                if post.id == key {
+                    self.posts.remove(at: index)
+                }
+            } */
+            /// this filter is more efficient, and same as above for loop
+            self.posts = self.posts.filter { $0.id != post.id }
+            /// real time query for posts and users count on follow and unfollow
+            self.users = self.users.filter { $0.id != post.uid }
+            self.tableView.reloadData()
+        })
+        
+    }
+    
+    /// it's job is to...
+    /// given a user id, look up the corresponding user on db...
+    func fetchUser(uid: String, completed: @escaping () -> Void) {
+        
+        Api.UserDet.observeUser(withId: uid, completion: { (user) in
+            self.users.append(user)
+            completed()
+        })
+        
+    }
+    
+}
+
+extension FeedViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! HomeTableViewCell
+        cell.backgroundColor = UIColor.white
+        let post = posts[indexPath.row]
+        let user = users[indexPath.row]
+        cell.post = post
+        cell.user = user
+        // linking home VC & home table view cell
+        cell.homeVC = self
+        return cell
+    }
+    
+}   // #114
