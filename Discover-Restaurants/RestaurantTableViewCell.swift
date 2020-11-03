0beabda4cc82c636a675be678925eb904095cbe5
likhitgarimella <likhitgmella@gmail.com>
2020-11-03 16:58:38 +0530

    disc rest - rest tbl view cell file added

---
+++ Discover-Restaurants/RestaurantTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  RestaurantTableViewCell.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 03/11/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        
+        
+    }
+    
+}   // #28
