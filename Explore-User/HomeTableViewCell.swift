7b9bbfc5db5f367ccb13aed8328cbcbaf9480b8a
likhitgarimella <likhitgmella@gmail.com>
2020-11-03 12:02:22 +0530

    home tbl view cell file added; cell storyboard changes #1

---
+++ Explore-User/HomeTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  HomeTableViewCell.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 03/11/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        
+        
+    }
+
+}   // #28
