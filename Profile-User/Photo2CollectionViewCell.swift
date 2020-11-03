32251869e11938f4fcce992a35639aa6a5f0afe0
likhitgarimella <likhitgmella@gmail.com>
2020-11-03 23:47:19 +0530

    user profile -> xib cell & cell file added

---
+++ Profile-User/Photo2CollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  Photo2CollectionViewCell.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 03/11/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class Photo2CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+}   // #21
