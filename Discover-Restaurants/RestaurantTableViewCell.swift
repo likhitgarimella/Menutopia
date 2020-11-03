1145d9bdec86fa7257df0945c195d9d932fad27d
likhitgarimella <likhitgmella@gmail.com>
2020-11-03 18:44:14 +0530

    rest tbl view cell -> outlets connected; file code done

--- Discover-Restaurants/RestaurantTableViewCell.swift
+++ Discover-Restaurants/RestaurantTableViewCell.swift
@@ -10,6 +10,38 @@ import UIKit
 
 class RestaurantTableViewCell: UITableViewCell {
     
+    // Outlets
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    @IBOutlet var contactButton: UIButton!
+    
+    var restaurant: AppUser? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    func updateView() {
+        
+        nameLabel.text = restaurant?.restaurantName
+        
+        configureContactButton()
+        
+    }
+    
+    func configureContactButton() {
+        
+        // contactButton.layer.borderWidth = 1
+        // contactButton.layer.borderColor = UIColor(red: 226/255, green: 228/255, blue: 232/255, alpha: 1).cgColor
+        contactButton.layer.cornerRadius = 5
+        contactButton.clipsToBounds = true
+        ///
+        contactButton.setTitleColor(UIColor.white, for: .normal)
+        contactButton.backgroundColor = UIColor(red: 69/255, green: 142/255, blue: 255/255, alpha: 1)
+        contactButton.setTitle("Contact", for: .normal)
+        
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -24,4 +56,4 @@ class RestaurantTableViewCell: UITableViewCell {
         
     }
     
-}   // #28
+}   // #60
