6fe8a90d0cda9f8227d41c1396e8c6420cf24120
likhitgarimella <likhitgmella@gmail.com>
2020-11-20 05:37:28 +0530

    continue screen; description & content; disable back

---
+++ Set-1/DescriptionViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DescriptionViewController.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 20/11/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class DescriptionViewController: UIViewController {
+    
+    @IBAction func dismiss(_ sender: UIButton) {
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // 27
