3fd4d5813df81f4c5b9776501c5357646d32ce3a
likhitgarimella <likhitgmella@gmail.com>
2020-11-24 10:54:12 +0530

    edit profle classes & files added for rest & user

---
+++ Profile-Restaurant/EditRestaurantProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EditRestaurantProfileViewController.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 24/11/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class EditRestaurantProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
---
+++ Profile-User/EditUserProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EditUserProfileViewController.swift
+//  Snapfood
+//
+//  Created by Likhit Garimella on 24/11/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class EditUserProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
