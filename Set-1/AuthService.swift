bd5284357dc2126d889e086f103d224b53c3ed06
likhitgarimella <likhitgmella@gmail.com>
2020-11-29 05:33:26 +0530

    RestSignupVC & AuthService - rest -> complete rest signup completed - prof pic + other 4 details; rest signup VC tfs all 3 prop funcs

--- Set-1/AuthService.swift
+++ Set-1/AuthService.swift
@@ -9,6 +9,7 @@
 import Foundation
 import FirebaseAuth
 import FirebaseDatabase
+import FirebaseStorage
 
 class AuthService {
     
@@ -28,7 +29,7 @@ class AuthService {
     }
     
     // Restaurant Sign up
-    static func restaurantSignUp(name: String, email: String, password: String, onSuccess: @escaping () -> Void, onError: @escaping (_ errorMessage: String?) -> Void) {
+    static func restaurantSignUp(name: String, email: String, address: String, cityState: String, phoneNo: String, openHours: String, password: String, imageData: Data, onSuccess: @escaping () -> Void, onError: @escaping (_ errorMessage: String?) -> Void) {
         
         print("Restaurant Sign up")
         // Firebase Auth
@@ -43,17 +44,37 @@ class AuthService {
                 return
             }
             
-            self.setRestaurantInformation(name: name, email: email, uid: uid, onSuccess: onSuccess)
+            // Firebase Storage
+            // reference url
+            let storageRef = Storage.storage().reference(forURL: "gs://snapfood-2304acc.appspot.com")
+            let storageProfileRef = storageRef.child("RestaurantProfilePics").child(uid)
+            let metadata = StorageMetadata()
+            metadata.contentType = "image/jpg"
+            // put image data
+            storageProfileRef.putData(imageData, metadata: metadata) { (storageMetaData, error) in
+                if error != nil {
+                    print(error!.localizedDescription)
+                    return
+                }
+                // get download url for image from Firebase Storage
+                storageProfileRef.downloadURL { (url, error) in
+                    // convert that download url to string
+                    if let metaImageUrl = url?.absoluteString {
+                        print(metaImageUrl)
+                        self.setRestaurantInformation(profileImageUrl: metaImageUrl, name: name, email: email, address: address, cityState: cityState, phoneNo: phoneNo, openHours: openHours, uid: uid, onSuccess: onSuccess)
+                    }
+                }
+            }
             
         })
         
     }
     
     // Set restaurant info to database
-    static func setRestaurantInformation(name: String, email: String, uid: String, onSuccess: @escaping () -> Void) {
+    static func setRestaurantInformation(profileImageUrl: String, name: String, email: String, address: String, cityState: String, phoneNo: String, openHours: String, uid: String, onSuccess: @escaping () -> Void) {
         
         let databaseRefRest = Database.database().reference().child("Restaurants").child(uid)
-        databaseRefRest.setValue(["1) Restaurant name": name, "2) Restaurant email": email])
+        databaseRefRest.setValue(["1) Restaurant name": name, "2) Restaurant email": email, "3) Restaurant address": address, "4) Restaurant city & state": cityState, "5) Restaurant phone": phoneNo, "6) Restaurant open hours": openHours, "7) Restaurant profile photo url": profileImageUrl])
         onSuccess()
         
     }
@@ -101,4 +122,4 @@ class AuthService {
         
     }
     
-}   // #105
+}   // #126
--- Set-1/RestaurantSignupViewController.swift
+++ Set-1/RestaurantSignupViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+// import Firebase
 import JGProgressHUD
 
 class RestaurantSignupViewController: UIViewController {
@@ -20,12 +21,26 @@ class RestaurantSignupViewController: UIViewController {
     @IBOutlet var restOpenHours: UITextField!
     @IBOutlet var password: UITextField!
     @IBOutlet var confirmPassword: UITextField!
+    
+    @IBOutlet var profileImage: UIImageView!
+    
     @IBOutlet var register: UIButton!
+    
+    // global variable for selected image
+    var selectedImage: UIImage?
+    
+    // image that appears on screen as profile image for restaurant
+    // an Optional
+    var image: UIImage? = nil
     
     func Properties() {
         
         restName.backgroundColor = UIColor(red: 252/255, green: 239/255, blue: 238/255, alpha: 1)
         restEmail.backgroundColor = UIColor(red: 252/255, green: 239/255, blue: 238/255, alpha: 1)
+        restAddress.backgroundColor = UIColor(red: 252/255, green: 239/255, blue: 238/255, alpha: 1)
+        restCityState.backgroundColor = UIColor(red: 252/255, green: 239/255, blue: 238/255, alpha: 1)
+        restPhoneNo.backgroundColor = UIColor(red: 252/255, green: 239/255, blue: 238/255, alpha: 1)
+        restOpenHours.backgroundColor = UIColor(red: 252/255, green: 239/255, blue: 238/255, alpha: 1)
         password.backgroundColor = UIColor(red: 252/255, green: 239/255, blue: 238/255, alpha: 1)
         confirmPassword.backgroundColor = UIColor(red: 252/255, green: 239/255, blue: 238/255, alpha: 1)
         
@@ -35,6 +50,10 @@ class RestaurantSignupViewController: UIViewController {
         
         restName.layer.cornerRadius = 20
         restEmail.layer.cornerRadius = 20
+        restAddress.layer.cornerRadius = 20
+        restCityState.layer.cornerRadius = 20
+        restPhoneNo.layer.cornerRadius = 20
+        restOpenHours.layer.cornerRadius = 20
         password.layer.cornerRadius = 20
         confirmPassword.layer.cornerRadius = 20
         register.layer.cornerRadius = 20
@@ -47,10 +66,45 @@ class RestaurantSignupViewController: UIViewController {
         restName.leftViewMode = .always
         restEmail.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: restEmail.frame.height))
         restEmail.leftViewMode = .always
+        restAddress.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: restAddress.frame.height))
+        restAddress.leftViewMode = .always
+        restCityState.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: restCityState.frame.height))
+        restCityState.leftViewMode = .always
+        restPhoneNo.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: restPhoneNo.frame.height))
+        restPhoneNo.leftViewMode = .always
+        restOpenHours.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: restOpenHours.frame.height))
+        restOpenHours.leftViewMode = .always
         password.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: password.frame.height))
         password.leftViewMode = .always
         confirmPassword.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: confirmPassword.frame.height))
         confirmPassword.leftViewMode = .always
+        
+    }
+    
+    func ProfileImage() {
+        
+        // Profile image properties
+        profileImage.layer.cornerRadius = 40
+        profileImage.layer.masksToBounds = true
+        
+        // Add gesture for profile image present in screen
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleSelectProfileImageView))
+        profileImage.addGestureRecognizer(tapGesture)
+        profileImage.isUserInteractionEnabled = true
+        
+    }
+    
+    @objc func handleSelectProfileImageView() {
+        
+        let pickerController = UIImagePickerController()
+        // To get access to selected media files, add delegate
+        pickerController.delegate = self
+        /// presenting it in full screen bcuz...
+        /// i want the view to change...
+        /// so that viewWillAppear will work...
+        pickerController.modalPresentationStyle = .fullScreen
+        // present photo library
+        present(pickerController, animated: true, completion: nil)
         
     }
 
@@ -62,6 +116,7 @@ class RestaurantSignupViewController: UIViewController {
         Properties()
         CornerRadius()
         LeftPadding()
+        ProfileImage()
         
     }
     
@@ -76,6 +131,9 @@ class RestaurantSignupViewController: UIViewController {
     
     @IBAction func registerTapped(_ sender: UIButton) {
         
+        // dismiss keyboard
+        view.endEditing(true)
+        
         hud1.show(in: self.view)
         
         func isValidEmail(testStr:String) -> Bool {
@@ -84,20 +142,34 @@ class RestaurantSignupViewController: UIViewController {
             return emailTest.evaluate(with: testStr)
         }
         
-        if (restName.text?.isEmpty == false && restEmail.text?.isEmpty == false && password.text?.isEmpty == false && confirmPassword.text?.isEmpty == false) {
+        if (restName.text?.isEmpty == false && restEmail.text?.isEmpty == false && restAddress.text?.isEmpty == false && restCityState.text?.isEmpty == false && restPhoneNo.text?.isEmpty == false && restOpenHours.text?.isEmpty == false && password.text?.isEmpty == false && confirmPassword.text?.isEmpty == false) {
             
             if (password.text == confirmPassword.text) {
                 
                 if(isValidEmail(testStr: restEmail.text!)) {
                     
                     // Validations
-                    guard let name = restName.text, let email = restEmail.text, let password = password.text else {
+                    guard let name = restName.text, let email = restEmail.text, let address = restAddress.text, let cityState = restCityState.text, let phoneNo = restPhoneNo.text, let openHours = restOpenHours.text, let password = password.text else {
                         print("Invalid Form Input")
                         return
                     }
                     
+                    // selected image should be from image
+                    guard let imageSelected = self.image else {
+                        print("Avatar is nil")
+                        hud1.indicatorView = nil    // remove indicator
+                        hud1.textLabel.text = "Profile image can't be empty"
+                        hud1.dismiss(afterDelay: 2.0, animated: true)
+                        return
+                    }
+                    
+                    // image data from selected image in jpeg format & compression
+                    guard let imageData = imageSelected.jpegData(compressionQuality: 0.4) else {
+                        return
+                    }
+                    
                     // Auth service sign up
-                    AuthService.restaurantSignUp(name: name, email: email, password: password, onSuccess: {
+                    AuthService.restaurantSignUp(name: name, email: email, address: address, cityState: cityState, phoneNo: phoneNo, openHours: openHours, password: password, imageData: imageData, onSuccess: {
                         print("On Success")
                         self.hud1.show(in: self.view)
                         self.hud1.indicatorView = nil
@@ -138,4 +210,30 @@ class RestaurantSignupViewController: UIViewController {
         
     }
     
-}   // #142
+}
+
+extension RestaurantSignupViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // Selected image to display it in our profile image
+        if let imageSelected = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            // set profile image's imageView to selected image
+            profileImage.image = imageSelected
+            // Store this img in an instance variable
+            image = imageSelected
+        }
+        // Original image
+        if let imageOriginal = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            // set profile image's imageView to original image
+            profileImage.image = imageOriginal
+            // Store this img in an instance variable
+            image = imageOriginal
+        }
+        
+        print("Image selected from library")
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+}   // #240
